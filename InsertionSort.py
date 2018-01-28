988bff716ce44118a2f6c9cf45b9c0eedd668942
neelkirit <neelkirit2@gmail.com>
2018-01-28 23:06:29 +0530

    Insertion Sort Increasing Order

--- InsertionSort.py
+++ InsertionSort.py
@@ -5,7 +5,7 @@ print '------------------------'
 for i in range(1,len(a)):
     key = a[i]
     j = i - 1
-    while (j>=0) and (a[j]<key):
+    while (j>=0) and (a[j]>key):
         a[j+1]=a[j]
         j=j-1
     a[j+1] = key
